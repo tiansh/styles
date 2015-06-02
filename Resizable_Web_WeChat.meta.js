cdf5dfd6bc28ac29f2fb17a5843980de2d7f2b03
tiansh <tiansheng111@gmail.com>
2015-06-02 14:52:47 +1000

    init

---
+++ Resizable_Web_WeChat.meta.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name              Resizable Web WeChat
+// @name:zh-CN        网页微信适应窗口大小
+// @name:zh-TW        網頁 WeChat 適應視窗大小 
+// @name:en           Resizable Web WeChat
+// @namespace         https://github.com/tiansh
+// @description       根据浏览器窗口大小，自动调整网页微信的排版。让微信网页不再出现恼人的滚动条
+// @description:zh-CN 根据浏览器窗口大小，自动调整网页微信的排版。让微信网页不再出现恼人的滚动条
+// @description:zh-TW 根據瀏覽器視窗大小，自動配置網頁 WeChat 的排版。讓 WeChat 網頁不再出現惱人的捲軸
+// @description:en    Automatically adjust Web WeChat layout according to the window size, so that the annoying scroll bar won't bother you anymore
+// @include           https://wx.qq.com/*
+// @exclude           https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxreadtemplate*
+// @version           1.0
+// @updateURL         https://tiansh.github.io/us-wechat/Resizable_Web_WeChat.meta.js
+// @downloadURL       https://tiansh.github.io/us-wechat/Resizable_Web_WeChat.user.js
+// @homepageURL       https://tiansh.github.io/us-wechat/Resizable_Web_WeChat.html
+// @supportURL        https://github.com/tiansh/us-wechat/issues
+// @author            田生
+// @copyright         田生; MPL 2.0
+// @license           Mozilla Public License, version 2.0; https://www.mozilla.org/MPL/2.0/
+// @grant             GM_addStyle
+// @run-at            document-start
+// ==/UserScript==
